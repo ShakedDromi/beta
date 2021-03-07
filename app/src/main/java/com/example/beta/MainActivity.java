d82b8e7df72728b8f2fee8483bb9cd33be3b7176
ShskedDromi <sd5308@bs.amalnet.k12.il>
2021-03-07 12:41:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/beta/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.beta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
---
+++ app/src/main/java/com/example/beta/TAG.java
@@ -0,0 +1,4 @@
+package com.example.beta;
+
+public class TAG {
+}
